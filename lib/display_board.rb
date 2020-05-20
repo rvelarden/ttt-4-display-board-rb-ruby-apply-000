5d14ec59ee41e9bacc9442578f7acd6026438838
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-20 00:42:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+row = [" ", " ", " "]
+
+output = capture_puts{ display_board(board) }
+rows = output.split("\n")
+
+
+row[0]
+
+
+row[1]
+
+
+row[2]
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
+end
